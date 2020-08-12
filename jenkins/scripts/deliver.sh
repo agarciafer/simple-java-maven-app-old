9fa9c4120b2394b1af5474eeb9b7e40842e80bb4
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2020-08-12 18:54:18 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -25,5 +25,5 @@ echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
 echo "Copiar a 192.168.33.11"
-scp /jenkins-docker/jenkins_home/workspace/pipeline-2/target 192.168.33.11:/tmp
+scp /var/jenkins_home/workspace/maven-jobs/target/${NAME}-${VERSION}.jar 192.168.33.11:/tmp
 java -jar 192.168.33.11:/tmp/${NAME}-${VERSION}.jar > /tmp/salida${NAME}-${VERSION}
