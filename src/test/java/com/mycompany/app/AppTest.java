b2d276b029a50f2c845b81e59836c47d3ce4ed13
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2022-06-15 12:18:57 +0200

    v2

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World-v1!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World-v2!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World-v2!\"");
         }
