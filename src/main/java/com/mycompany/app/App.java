a77caaf374374ee059031d32384da1b1fea7c6cb
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2022-07-22 13:23:18 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World-v4!";
+    private final String message = "Hello World-v5!";
 
     public App() {}
 
